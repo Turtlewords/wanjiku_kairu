f9bb8d229d623fa019245f5782bb7db0bd187613
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-09-03 20:19:53 -0500

    begin slide menu

---
+++ index.js
@@ -0,0 +1,23 @@
+// Elements
+
+const container = document.querySelector(".container")
+const hamburgerOpen = document.querySelector(".header__hamburger")
+const hamburgerClose = document.querySelector(".menu__close-btn")
+const slideMenu = document.querySelector(".menu__container")
+const slideInnerMenu = document.querySelector(".menu__inner-container")
+
+// Event Listeners
+
+hamburgerOpen.addEventListener("click", () => {
+    hamburgerOpen.classList.add("invisible")
+    slideMenu.classList.add("w-3/4")
+    container.classList.add("opacity-50")
+    
+})
+
+hamburgerClose.addEventListener("click", () => {
+    hamburgerOpen.classList.remove("invisible")
+    slideMenu.classList.remove("w-3/4")
+    container.classList.remove("opacity-50")
+})
+
