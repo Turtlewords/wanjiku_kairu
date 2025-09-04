ef9725748d25b6381e7521e83ff58e035f4fc14b
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-09-04 18:37:13 -0500

    begin linking pages

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const hamburgerClose = document.querySelector(".menu__close-btn")
 const slideMenu = document.querySelector(".menu__container")
 const slideInnerMenu = document.querySelector(".menu__inner-container")
 
+const slideMenuLinks = document.querySelectorAll(".slide-menu__link")
+
 // Event Listeners
 
 hamburgerOpen.addEventListener("click", () => {
@@ -19,5 +21,12 @@ hamburgerClose.addEventListener("click", () => {
     hamburgerOpen.classList.remove("invisible")
     slideMenu.classList.remove("w-3/4")
     container.classList.remove("opacity-50")
+})
+
+slideMenuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+    slideMenu.classList.remove("w-3/4")
+    hamburgerOpen.classList.remove("invisible")
+    })
 })
 
