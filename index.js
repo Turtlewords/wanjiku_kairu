87219a2c9b0ca03d5076f0153e519395cc338be7
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-09-06 16:34:37 -0500

    animate gallery

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hamburgerOpen = document.querySelector(".header__hamburger")
 const hamburgerClose = document.querySelector(".menu__close-btn")
 const slideMenu = document.querySelector(".menu__container")
 const slideInnerMenu = document.querySelector(".menu__inner-container")
+const galleryImgs = document.querySelectorAll(".gallery__img")
 
 const slideMenuLinks = document.querySelectorAll(".slide-menu__link")
 
@@ -28,6 +29,18 @@ slideMenuLinks.forEach((link) => {
     slideMenu.classList.remove("w-3/4")
     hamburgerOpen.classList.remove("invisible")
     container.classList.remove("opacity-50")
+    })
+})
+
+galleryImgs.forEach((img) => {
+    img.addEventListener("mouseover", () => {
+        img.classList.add("scale-140")
+    })
+})
+
+galleryImgs.forEach((img) => {
+    img.addEventListener("mouseout", () => {
+        img.classList.remove("scale-140")
     })
 })
 
